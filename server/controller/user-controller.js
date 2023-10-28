10b5f2b97c19a55edec51af45506d7fc92db245b
rohan <panditrohan738@gmail.com>
2023-10-28 17:44:55 +0530

    first

---
+++ server/controller/user-controller.js
@@ -0,0 +1,9 @@
+export const signupUser = (request , response ) => {
+    try{
+
+    }
+    catch(error)
+    {
+
+    }
+}
--- server/index.js
+++ server/index.js
@@ -1,14 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import Connection from './database/db.js';
+import Router from './Routes/routes.js';
+
 
 dotenv.config();
 
 const app =express();
 
+
+app.use('/',Router);
+
+
 const port = 8000;
 
 app.listen(port,() => console.log( `server is running on port number ${port}`))
+
 
 
 const username = process.env.DB_USERNAME;
